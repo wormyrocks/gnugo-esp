a441c42814bc531a91e823558b8cff47ef6a9313
Evan Kahn <ekahn@hmc.edu>
2025-09-03 19:25:24 -0400

    lots of new stuff

--- interface/gnugo_esp_wrapper.c
+++ interface/gnugo_esp_wrapper.c
@@ -4,19 +4,23 @@
 #include "board.h"
 #include "liberty.h"
 #include "assert.h"
+#include "string.h"
 
 #define GRID_POINTS FIXED_BOARD_SIZE
 
 static Gameinfo gameinfo_;
 static Gameinfo *gameinfo = &gameinfo_;
+static char sgfname[128] = {0};
 static SGFTree sgftree;
-
 static esp_gnugo_game_state_t game_state;
 static board_update_callback update_cb;
+static int sgf_initialized = 0;
+static int passes = 0;
+static int game_is_over = 0;
+static bool undo_allowed = false;
 
-void esp_gnugo_update_board_state(int game_is_over)
+static void esp_gnugo_update_board_state()
 {
-    game_state.level = get_level();
     // Update board
     for (int x = 0; x < GRID_POINTS; x++)
         for (int y = 0; y < GRID_POINTS; y++)
@@ -32,7 +36,6 @@ void esp_gnugo_update_board_state(int game_is_over)
                 game_state.board[x][y] = val;
             }
         }
-    int last_move = get_last_move();
     game_state.state = ESP_GNUGO_STATE_WAITING_FOR_PLAYER;
     if (gameinfo->computer_player == gameinfo->to_move)
     {
@@ -43,7 +46,7 @@ void esp_gnugo_update_board_state(int game_is_over)
     {
         game_state.state = ESP_GNUGO_STATE_GAME_OVER;
         game_state.last_event = ESP_GNUGO_EVENT_WIN;
-        if (!is_pass(last_move))
+        if (!is_pass(get_last_move()))
         {
             game_state.last_event = ESP_GNUGO_EVENT_RESIGN;
         }
@@ -64,10 +67,10 @@ void esp_gnugo_update_board_state(int game_is_over)
     }
     else
     {
-        if (is_pass(last_move))
-        {
+        if (movenum == 0)
+            game_state.last_event = ESP_GNUGO_EVENT_NONE;
+        else if (is_pass(get_last_move()))
             game_state.last_event = ESP_GNUGO_EVENT_PASS;
-        }
         else
         {
             game_state.last_event = ESP_GNUGO_EVENT_PLAY;
@@ -77,27 +80,61 @@ void esp_gnugo_update_board_state(int game_is_over)
             }
         }
         if (movenum)
-            location_to_buffer(last_move, (gameinfo->to_move == BLACK) ? game_state.white_last_move : game_state.black_last_move);
+        {
+            location_to_buffer(get_last_move(), (gameinfo->to_move == BLACK) ? game_state.white_last_move : game_state.black_last_move);
+        }
     }
+    game_state.level = get_level();
     game_state.score = (white_score + black_score) / 2.0;
     game_state.white_turn = (gameinfo->to_move == WHITE);
     game_state.move_number = movenum;
     game_state.black_captured = black_captured;
     game_state.white_captured = white_captured;
-    // showboard(0);
-    // writesgf(sgftree.root, "out.sgf");
     if (update_cb != NULL)
         update_cb(&game_state);
+}
+
+static void esp_gnugo_init_board_state(char *infile, bool player_is_white, int requested_handicap)
+{
+    gameinfo_clear(gameinfo);
+    int did_load = 0;
+    if (infile)
+    {
+        printf("Trying to resume from %s\n", infile);
+        if (sgftree_readfile(&sgftree, infile))
+        {
+            did_load = (gameinfo_play_sgftree(gameinfo, &sgftree, NULL) != EMPTY);
+            if (did_load)
+                sgf_initialized = 1;
+            sgfOverwritePropertyInt(sgftree.root, "HA", gameinfo->handicap);
+        }
+    }
+    // Start fresh
+    if (!did_load)
+    {
+        gameinfo->computer_player = (player_is_white ? BLACK : WHITE);
+        gameinfo->handicap = requested_handicap;
+        handicap = gameinfo->handicap;
+        gameinfo->to_move = BLACK;
+        if (gameinfo->handicap != 0)
+        {
+            gameinfo->handicap = place_fixed_handicap(gameinfo->handicap);
+            gameinfo->to_move = WHITE;
+        }
+        sgf_initialized = 0;
+        sgftreeCreateHeaderNode(&sgftree, board_size, komi, gameinfo->handicap);
+    }
+    gameinfo->game_record = sgftree;
+    white_score = 0;
+    black_score = 0;
+    memset(&game_state, 0, sizeof(esp_gnugo_game_state_t));
+    esp_gnugo_update_board_state();
 }
 
 void esp_gnugo_set_level(int level)
 {
     set_level(level);
 }
-
-static int sgf_initialized = 0;
-static int passes = 0;
-static bool undo_allowed = false;
 
 static void
 init_sgf(Gameinfo *ginfo)
@@ -107,16 +144,13 @@ init_sgf(Gameinfo *ginfo)
     sgf_initialized = 1;
     sgf_write_header(sgftree.root, 1, get_random_seed(), komi,
                      ginfo->handicap, get_level(), chinese_rules);
+    if (ginfo->handicap > 0)
+        sgffile_recordboard(sgftree.root);
 }
-
-void esp_gnugo_save_sgf(char *fname)
+static esp_gnugo_game_init_t i_p;
+esp_gnugo_state_t esp_gnugo_start(esp_gnugo_game_init_t init_params)
 {
-    init_sgf(gameinfo);
-    writesgf(sgftree.root, fname);
-}
-
-void esp_gnugo_start(esp_gnugo_game_init_t init_params)
-{
+    memcpy(&i_p, &init_params, sizeof(esp_gnugo_game_init_t));
     assert(game_state.state == ESP_GNUGO_STATE_NOT_STARTED);
     passes = 0;
     autolevel_on = init_params.autolevel;
@@ -125,38 +159,20 @@ void esp_gnugo_start(esp_gnugo_game_init_t init_params)
     set_level(init_params.start_level);
     komi = init_params.komi;
     board_size = FIXED_BOARD_SIZE;
-    gameinfo_clear(gameinfo);
-    gameinfo->handicap = init_params.requested_handicap;
     choose_mc_patterns("montegnu_classic");
     gnugo_clear_board(board_size);
     init_gnugo(init_params.memory_mb, init_params.random_seed);
-    if (init_params.sgftree)
+    // outfilename: autosave file. default to stdout
+    strcpy(sgfname, "-");
+
+    // init_params.outfile: user saves this explicitly
+    if (init_params.outfile)
     {
-        assert(sgftree_readfile(&sgftree, init_params.sgftree));
-        assert(gameinfo_play_sgftree(gameinfo, &sgftree, NULL) != EMPTY);
+        strcpy(sgfname, init_params.outfile);
+        printf("Game may be manually saved to %s\n", init_params.outfile);
     }
-    else
-    {
-        gameinfo->computer_player = (init_params.player_is_white ? BLACK : WHITE);
-        gameinfo->handicap = init_params.requested_handicap;
-        gameinfo->to_move = BLACK;
-        if (gameinfo->handicap != 0)
-        {
-            gameinfo->handicap = place_fixed_handicap(gameinfo->handicap);
-            gameinfo->to_move = WHITE;
-        }
-        sgftreeCreateHeaderNode(&sgftree, board_size, komi, handicap);
-    }
-    gameinfo->game_record = sgftree;
-    if (gameinfo->computer_player == gameinfo->to_move)
-    {
-        game_state.state = ESP_GNUGO_STATE_WAITING_FOR_CPU;
-    }
-    else
-    {
-        game_state.state = ESP_GNUGO_STATE_WAITING_FOR_PLAYER;
-    }
-    esp_gnugo_update_board_state(0);
+    esp_gnugo_init_board_state(init_params.infile, init_params.player_is_white, init_params.requested_handicap);
+    return game_state.state;
 }
 
 static void process_move(int move, int did_resign)
@@ -169,70 +185,67 @@ static void process_move(int move, int did_resign)
         passes++;
     else
         passes = 0;
-    if (passes >= 2 || did_resign)
+    game_is_over = (passes >= 2 || did_resign);
+    if (game_is_over)
     {
         if (passes >= 2)
         {
             who_wins(EMPTY, stdout);
         }
-        printf("***GAME OVER***\n\n\n");
-        esp_gnugo_update_board_state(1);
     }
-    else
-    {
-        esp_gnugo_update_board_state(0);
-    }
+    esp_gnugo_update_board_state();
 }
 
-// return 1 if ok, 0 if illegal, -1 if ???
-int esp_gnugo_set_player_move_str(char *in_move)
+// return 1? check the game state
+// return 0? nothing change, engine expects another command
+int esp_gnugo_set_player_command(engine_signal_t e)
 {
-    int out_move = 0, resign = 0;
-    if (in_move[0] == 'p')
+    go_command_t go_command = e.cmd;
+    int move_if_any = e.pos;
+    if (game_is_over && go_command <= COMMAND_RESIGN)
+        return 0;
+    switch (go_command)
     {
-        in_move = PASS_MOVE;
+    case COMMAND_PASS:
+    case COMMAND_RESIGN:
+        process_move(0, (go_command == COMMAND_RESIGN));
+        return 1;
+    case COMMAND_PLAY:
+    {
+        int ret = is_allowed_move(move_if_any, gameinfo->to_move);
+        if (ret)
+            process_move(move_if_any, 0);
+        return ret;
     }
-    else if (in_move[0] == 'r')
+    case COMMAND_SAVE:
     {
-        resign = 1;
-    }
-    else
-    {
-        out_move = string_to_location(FIXED_BOARD_SIZE, in_move);
-        if (out_move == NO_MOVE || !is_allowed_move(out_move, gameinfo->to_move))
-        {
-            printf("illegal\n");
-            return 0;
-        }
-    }
-    process_move(out_move, resign);
-    return 1;
-}
-
-// return 1 if ok, 0 if illegal, -1 if ???
-int esp_gnugo_set_player_move_int(int in_move)
-{
-    int out_move = 0, resign = 0;
-    if (!is_allowed_move(in_move, gameinfo->to_move))
-    {
-        printf("illegal\n");
+        printf("Game saved to %s\n", sgfname);
+        init_sgf(gameinfo);
+        writesgf(sgftree.root, sgfname);
         return 0;
     }
-    process_move(in_move, resign);
-    return 1;
+    case COMMAND_RESTART:
+        esp_gnugo_restart();
+        return 1;
+    case COMMAND_FORCEQUIT:
+        return 1;
+    }
+    return game_state.state;
 }
 
 void esp_gnugo_restart()
 {
     passes = 0;
+    game_is_over = 0;
     sgfFreeNode(sgftree.root);
     sgftree_clear(&sgftree);
-    sgftreeCreateHeaderNode(&sgftree, board_size, komi, gameinfo->handicap);
-    sgf_initialized = 0;
+    sgftreeCreateHeaderNode(&sgftree, board_size, komi, i_p.requested_handicap);
     gameinfo_clear(gameinfo);
+    // Restarting should ignore the input filename arg
+    esp_gnugo_init_board_state(NULL, i_p.player_is_white, i_p.requested_handicap);
 }
 
-void esp_gnugo_get_computer_move()
+esp_gnugo_state_t esp_gnugo_get_computer_move()
 {
     init_sgf(gameinfo);
     adjust_level_offset(gameinfo->to_move);
@@ -240,9 +253,20 @@ void esp_gnugo_get_computer_move()
     float move_value;
     int move = genmove(gameinfo->to_move, &move_value, &resign);
     process_move(move, resign);
+    return game_state.state;
 }
 
 int esp_gnugo_pos_from_xy(int x, int y)
 {
     return POS(x, y);
+}
+
+esp_gnugo_game_state_t *esp_gnugo_get_game_state()
+{
+    return &game_state;
+}
+
+esp_gnugo_state_t esp_gnugo_get_state()
+{
+    return game_state.state;
 }
--- interface/gnugo_esp_wrapper.h
+++ interface/gnugo_esp_wrapper.h
@@ -13,22 +13,19 @@
 
 #define GRID_HOSHI 10
 
-typedef enum {
+// Engine => UI
+typedef enum
+{
     ESP_GNUGO_STATE_NOT_STARTED,
     ESP_GNUGO_STATE_WAITING_FOR_PLAYER,
     ESP_GNUGO_STATE_WAITING_FOR_CPU,
     ESP_GNUGO_STATE_GAME_OVER,
 } esp_gnugo_state_t;
 
-typedef enum {
-    ESP_GNUGO_MOVE,
-    ESP_GNUGO_PASS,
-    ESP_GNUGO_RESIGN,
-    ESP_GNUGO_UNDO
-} esp_gnugo_move_type_t;
-
-// Events that may include animations
-typedef enum {
+// Engine => UI
+typedef enum
+{
+    ESP_GNUGO_EVENT_NONE,
     ESP_GNUGO_EVENT_PLAY,
     ESP_GNUGO_EVENT_ILLEGAL,
     ESP_GNUGO_EVENT_CAPTURE,
@@ -39,7 +36,9 @@ typedef enum {
     ESP_GNUGO_EVENT_RESTART
 } esp_gnugo_event_t;
 
-typedef struct {
+// Engine => UI
+typedef struct
+{
     uint8_t board[9][9];
     char white_last_move[20];
     char black_last_move[20];
@@ -52,9 +51,32 @@ typedef struct {
     esp_gnugo_state_t state;
     esp_gnugo_event_t last_event;
 } esp_gnugo_game_state_t;
-typedef void (*board_update_callback)(const esp_gnugo_game_state_t*);
 
-typedef struct {
+// UI => Engine
+typedef enum
+{
+    COMMAND_NONE,
+    COMMAND_GENMOVE,
+    COMMAND_PASS,
+    COMMAND_PLAY,
+    COMMAND_RESIGN,
+    COMMAND_RESTART,
+    COMMAND_SAVE,
+    COMMAND_FORCEQUIT
+} go_command_t;
+
+// UI => Engine
+typedef struct __attribute__((packed))
+{
+    go_command_t cmd : 16;
+    int pos : 16;
+} engine_signal_t;
+
+// UI => Engine
+typedef void (*board_update_callback)(const esp_gnugo_game_state_t *);
+// UI => Engine
+typedef struct
+{
     bool player_is_white;
     bool undo_allowed;
     bool autolevel;
@@ -63,16 +85,17 @@ typedef struct {
     int random_seed;
     int memory_mb;
     int requested_handicap;
-    char* sgftree;
+    char *infile;
+    char *outfile;
     board_update_callback update_callback;
 } esp_gnugo_game_init_t;
 
-void esp_gnugo_set_level(int level);
-void esp_gnugo_start(esp_gnugo_game_init_t);
-void esp_gnugo_get_computer_move();
-int esp_gnugo_set_player_move(char *);
-int esp_gnugo_set_player_move_int(int);
-int esp_gnugo_pos_from_xy(int x, int y);
-
-
+esp_gnugo_state_t esp_gnugo_start(esp_gnugo_game_init_t);
+esp_gnugo_state_t esp_gnugo_get_computer_move();
+int esp_gnugo_set_player_command(engine_signal_t);
+// void esp_gnugo_set_level(int level);
+// int esp_gnugo_set_player_move(char *);
+// int esp_gnugo_pos_from_xy(int x, int y);
+esp_gnugo_game_state_t *esp_gnugo_get_game_state();
+esp_gnugo_state_t esp_gnugo_get_state();
 #endif
